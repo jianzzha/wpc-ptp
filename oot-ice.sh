fad946e8a30a9dac9f92632e6b5bfcc9cf318cdf
Jianzhu Zhang <jianzzha@redhat.com>
2022-11-25 09:52:39 -0500

    updated oot-ice.sh; added deploy manifests
    Author:    Jinazhu Zhang <jianzzha@redhat.com>

--- oot-ice.sh
+++ oot-ice.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -eu
+set -eu -x
 
 exit_on_error () {
   local rc="$1"
@@ -40,6 +40,8 @@ build_image () {
   BASE_IMAGE='registry.access.redhat.com/ubi8-minimal:latest'
   TAG=${KERNEL_VER}
   DTK_IMAGE=$(oc adm release info --image-for=driver-toolkit quay.io/openshift-release-dev/ocp-release:${OCP_VER}-x86_64)
+  podman pull --authfile ${OPENSHIFT_SECRET_FILE} ${DTK_IMAGE}
+
   echo "Building for kernel:${KERNEL_VER} on OCP:${OCP_VER}"
   echo "DTKI for OCP:${OCP_VER} : ${DTK_IMAGE}"
 
@@ -143,6 +145,23 @@ if [ -z ${REGISTRY} ]; then
    echo "The environment variable REGISTRY must be set."
    exit 1
 fi
+
+OPENSHIFT_SECRET_FILE=pull-secrets.json
+oc get secret/pull-secret -n openshift-config --template='{{index .data ".dockerconfigjson" | base64decode}}' > ${OPENSHIFT_SECRET_FILE}
+REGISTRY_USER=${REGISTRY_USER:-""}
+REGISTRY_PASSWORD=${REGISTRY_PASSWORD:-""}
+if [[ -n ${REGISTRY_USER} && -n ${REGISTRY_PASSWORD} ]]; then
+    oc registry login --skip-check --registry="${REGISTRY}" --auth-basic="${REGISTRY_USER}:${REGISTRY_PASSWORD}" --to=${OPENSHIFT_SECRET_FILE}
+    oc set data secret/pull-secret -n openshift-config --from-file=.dockerconfigjson=${OPENSHIFT_SECRET_FILE}
+fi
+
+REGISTRY_CERT=${REGISTRY_CERT:-""}
+if [[ -n ${REGISTRY_CERT} && -f ${REGISTRY_CERT} ]]; then
+    oc delete configmap registry-cas -n openshift-config 2>/dev/null || true
+    oc create configmap registry-cas -n openshift-config --from-file=$(echo ${REGISTRY} | sed 's/:/.../')=${REGISTRY_CERT}
+    oc patch image.config.openshift.io/cluster --patch '{"spec":{"additionalTrustedCA":{"name":"registry-cas"}}}' --type=merge
+fi
+
 
 while getopts hrc:k:o:p ARG ; do
   case $ARG in
